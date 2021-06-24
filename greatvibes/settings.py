90d54c9132b4ebb3a67d79fb4b4f07decb18483c
Egesa <wayneotemahegesa@gmail.com>
2021-06-24 22:15:01 +0300

    routing to artist from index

--- greatvibes/settings.py
+++ greatvibes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@&^o-b4o=kvlnplzpngmkb7+aee9k5w05ef^fl_zypcjmq0&5*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['greatvibes-quick-meerkat-dp.eu-gb.mybluemix.net', 'localhost']
+ALLOWED_HOSTS = ['greatvibes-quick-meerkat-dp.eu-gb.mybluemix.net', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'greatvibes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,11 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
